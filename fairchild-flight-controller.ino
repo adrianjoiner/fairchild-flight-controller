fbb4013fe0b2a5b81f444aea78444421d0db75c0
Adrian Joiner <adrianjoiner@gmail.com>
2019-03-12 22:44:48 +0100

    [wip]

--- fairchild-flight-controller.ino
+++ fairchild-flight-controller.ino
@@ -6,9 +6,14 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Wire.begin();
+  Wire.setClock(400000); //Increase to fast I2C speed!
   delay(2000);
 
+  // scan for connected sensors, epecting to see ...
+  //    MPU9255 at 0x68
+  //    BMP-280 at 0x76
   ic2_scanner();
+
 
   
 
